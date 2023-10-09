a7c5a86e0b0ff7f5a59030afa45027643f733c28
Bear-8bit <76735372+Bear-8bit@users.noreply.github.com>
2023-10-10 01:03:19 +0700

    Add MenuControl In Admin (Add, Edit, Delete)

---
+++ DigitalStore/Areas/Admin/Controllers/MenuController.cs
@@ -0,0 +1,78 @@
+﻿using DigitalStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DigitalStore.Models.EF;
+
+namespace DigitalStore.Areas.Admin.Controllers
+{
+    
+    public class MenuController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Admin/Menu
+        public ActionResult Index()
+        {
+            var items = db.Menus;
+            return View(items);
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(Menu model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.CreatedDate = DateTime.Now;
+                model.ModifiedDate = DateTime.Now;
+                db.Menus.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var item = db.Menus.Find(id);
+            return View(item);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Menu model)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Menus.Attach(model);
+                model.ModifiedDate = DateTime.Now;
+                db.Entry(model).Property(x => x.Name).IsModified = true;
+                db.Entry(model).Property(x => x.Description).IsModified = true;
+                db.Entry(model).Property(x => x.ModifiedDate).IsModified = true;
+                db.Entry(model).Property(x => x.ModifiedBy).IsModified = true;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var item = db.Menus.Find(id);
+            if (item != null)
+            {
+                db.Menus.Remove(item);
+                db.SaveChanges();
+                return Json(new { success = true });
+            }
+            return Json(new { success = false });
+        }
+    }
+}
