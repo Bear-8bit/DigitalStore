d43441ee0b28fa7db27f0752862715a1c10f886a
Bear-8bit <76735372+Bear-8bit@users.noreply.github.com>
2023-10-10 01:36:10 +0700

    Add Publisher Control In Admin (Add, Edit, Delete)

---
+++ DigitalStore/Areas/Admin/Controllers/PublisherController.cs
@@ -0,0 +1,77 @@
+﻿using DigitalStore.Models;
+using DigitalStore.Models.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DigitalStore.Areas.Admin.Controllers
+{
+    public class PublisherController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Admin/Publisher
+        public ActionResult Index()
+        {
+            var item = db.Publishers;
+            return View(item);
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(Publisher model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.CreatedDate = DateTime.Now;
+                model.ModifiedDate = DateTime.Now;
+                db.Publishers.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var item = db.Publishers.Find(id);
+            return View(item);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Publisher model)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Publishers.Attach(model);
+                model.ModifiedDate = DateTime.Now;
+                db.Entry(model).Property(x => x.Name).IsModified = true;
+                db.Entry(model).Property(x => x.Description).IsModified = true;
+                db.Entry(model).Property(x => x.ModifiedDate).IsModified = true;
+                db.Entry(model).Property(x => x.ModifiedBy).IsModified = true;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var item = db.Publishers.Find(id);
+            if (item != null)
+            {
+                db.Publishers.Remove(item);
+                db.SaveChanges();
+                return Json(new { success = true });
+            }
+            return Json(new { success = false });
+        }
+    }
+}
