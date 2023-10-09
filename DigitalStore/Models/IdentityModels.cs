5526e340d1f4d4e5b9aad57507ef8624371fb40e
Bear-8bit <76735372+Bear-8bit@users.noreply.github.com>
2023-10-09 23:30:11 +0700

    Add CKEditor, CKFinder Add Admin Template Plugin, Style Add ConnectionString

--- DigitalStore/Models/IdentityModels.cs
+++ DigitalStore/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace DigitalStore.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("ChuoiKN", throwIfV1Schema: false)
         {
         }
 
