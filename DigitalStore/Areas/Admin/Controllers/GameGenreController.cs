a139b25785fad9217b7e52409396be80e6c425f6
Bear-8bit <76735372+Bear-8bit@users.noreply.github.com>
2023-10-10 14:13:53 +0700

    Add GameGenre Control (Add, Edit, Delete)

---
+++ DigitalStore/Areas/Admin/Controllers/GameGenreController.cs
@@ -0,0 +1,77 @@
+﻿using DigitalStore.Models;
+using DigitalStore.Models.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DigitalStore.Areas.Admin.Controllers
+{
+    public class GameGenreController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Admin/GameGenre
+        public ActionResult Index()
+        {
+            var item = db.GameGenres;
+            return View(item);
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(GameGenre model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.CreatedDate = DateTime.Now;
+                model.ModifiedDate = DateTime.Now;
+                db.GameGenres.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var item = db.GameGenres.Find(id);
+            return View(item);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(GameGenre model)
+        {
+            if (ModelState.IsValid)
+            {
+                db.GameGenres.Attach(model);
+                model.ModifiedDate = DateTime.Now;
+                db.Entry(model).Property(x => x.Name).IsModified = true;
+                db.Entry(model).Property(x => x.Description).IsModified = true;
+                db.Entry(model).Property(x => x.ModifiedDate).IsModified = true;
+                db.Entry(model).Property(x => x.ModifiedBy).IsModified = true;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var item = db.GameGenres.Find(id);
+            if (item != null)
+            {
+                db.GameGenres.Remove(item);
+                db.SaveChanges();
+                return Json(new { success = true });
+            }
+            return Json(new { success = false });
+        }
+    }
+}
