aa033cddb4c697c4c3d16f6519a5025a121c95da
Bear-8bit <76735372+Bear-8bit@users.noreply.github.com>
2023-10-15 23:20:31 +0700

    Modified Database

---
+++ DigitalStore/Migrations/202310151619595_AddAlias.cs
@@ -0,0 +1,26 @@
+﻿namespace DigitalStore.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddAlias : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.tb_GameGenre", "Alias", c => c.String());
+            AddColumn("dbo.tn_Game", "Alias", c => c.String());
+            AddColumn("dbo.tb_GameNews", "Alias", c => c.String());
+            AddColumn("dbo.tb_NewsCategory", "Alias", c => c.String());
+            AddColumn("dbo.tb_Publisher", "Alias", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.tb_Publisher", "Alias");
+            DropColumn("dbo.tb_NewsCategory", "Alias");
+            DropColumn("dbo.tb_GameNews", "Alias");
+            DropColumn("dbo.tn_Game", "Alias");
+            DropColumn("dbo.tb_GameGenre", "Alias");
+        }
+    }
+}
