fa050cc5b2deeb9bf2957bfa18b63377c3d87bfd
Bear-8bit <76735372+Bear-8bit@users.noreply.github.com>
2023-10-25 02:52:02 +0700

    Add Login/Logoff (Customer)

---
+++ DigitalStore/Areas/Admin/Controllers/NewsController.cs
@@ -0,0 +1,20 @@
+﻿using DigitalStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DigitalStore.Areas.Admin.Controllers
+{
+    public class NewsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Admin/News
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
